0c81bf7458596df1cc59364d768c02df423b3c9d
ArnavRade <116534175+ArnavRade@users.noreply.github.com>
2026-03-24 14:54:05 +0530

    Refactor App class to Calculator and add add method
    Refactored App class into Calculator class and added add method.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,23 +1,6 @@
-public class App {
+public class Calculator {
 
-    public static void main(String[] args) {
-
-        String password = "123456"; // Hardcoded credential (security issue)
-
-        int unusedVariable = 10; // Unused variable
-
-        try {
-            int result = divide(10, 0); // Will cause exception
-        } catch (Exception e) {
-            // Empty catch block (bad practice)
-        }
-
-        if (true) { // Always true condition
-            System.out.println("This will always run");
-        }
-    }
-
-    public static int divide(int a, int b) {
-        return a / b;
+    public int add(int a, int b) {
+        return a + b;
     }
 }
